637f9ae4b2f91401ec44b62a637ee9bcc7fe7856
brianlin123 <95997248+brianlin123@users.noreply.github.com>
2025-11-09 19:12:19 +0800

    Maze class

--- DS_HW2.cpp
+++ DS_HW2.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+class Maze {
+ private:
+    int width;
+    int height;
+    char **map;
+ public:
+    Maze(int w, int h) : width(w), height(h) {
+        map = new char*[height];
+        for (int i = 0; i < height; ++i) {
+            map[i] = new char[width];
+        }
+    }
+
+    ~Maze() {
+        for (int i = 0; i < height; ++i) {
+            delete[] map[i];
+        }
+        delete[] map;
+    }
+
+    int getWidth() {
+        return width;
+    }
+
+    int getHeight() {
+        return height;
+    }
+    void display() const {
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                std::cout << map[i][j];
+            }
+            std::cout << std::endl;
+        }
+    }
+    //取單格的值
+    char getbox(int x, int y) {
+        return map[y][x];
+    }
+    //設單格的值
+    char setbox(int x, int y, char val) {
+        map[y][x] = val;
+    }
+};
+
+
 
 int main() {
-  std::cout << "Nigga" std::endl;
-    
+
+
+
 }
