d159414088f52302a3a7a25799dd69c5bece9c97
brianlin123 <brians8787@gmail.com>
2025-11-13 19:01:34 +0800

    findNgoals unfinished

--- DS_HW3.cpp
+++ DS_HW3.cpp
@@ -119,7 +119,38 @@ string getfile() {  // get filename from user
   cin >> filename;
   return "input" + filename + ".txt";
 }
-
+int getvalidN() {
+  int n;
+  std::string a;
+  while (1) {
+    std::cout << "\nNumber of G (goals): ";
+    std::cin >> a;
+    bool isvalid = true;
+    bool isnum = true;
+    for (int i = 0; i < a.length(); i++) {
+      if (a[i] < '0' || a[i] > '9') {
+        isnum = false;
+        isvalid = false;
+        break;
+      }
+    }
+    if (a.length() > 3) {
+      isvalid = false;
+      }
+    if (!isnum) {
+      continue;
+    } else if (!isvalid) {
+      std::cout << "\n### The number must be in [1,100] ###" << std::endl;
+      continue;
+    }
+    n = std::stoi(a);
+    if (n < 1 || n > 100) {
+    std::cout << "\n### The number must be in [1,100] ###" << std::endl;
+    } else {
+    return n;
+    }
+  }
+}
 void showMenu() {
   cout << "*** (^_^) Data Structure (^o^) ***" << endl;
   cout << "*** Find the Goal(s) in a Maze ***" << endl;
@@ -295,8 +326,82 @@ void findOneGoal() {
 }
 
 
-void findNGoals() { // task 2
-  cout << "2" << endl;
+void findNGoals(Maze* originMaze) { // task 2
+  if (originMaze == nullptr) {
+    cout << endl << "### Execute command 1 to load a maze! ###" << endl;
+    return;
+  }
+  int n = getvalidN();
+  int width = originMaze->getWidth();
+  int height = originMaze->getHeight();
+
+  //建立副本以防改動原本迷宮
+  Maze maze(width, height);
+  for (int row = 0; row < height; ++row) {
+    for (int col = 0; col < width; ++col) {
+      maze.setbox(col, row, originMaze->getbox(col, row));
+    }
+  }
+  //定義方向陣列 const以防不小心改到 右、下、左、上
+  const int dx[4] = {1, 0, -1, 0};
+  const int dy[4] = {0, 1, 0, -1};
+  const int baseOrder[4] = {0, 1, 2, 3};
+  
+  //建立兩個副本 一個紀錄走過位置 一個紀錄找到的目標位置
+  bool **visited = new bool*[height];
+  bool **goalvisited = new bool*[height];
+  for (int i = 0; i < height; i++) {
+    visited[i] = new bool[width];
+    goalvisited[i] = new bool[width];
+    for (int j = 0; j < width; j++) {
+      visited[i][j] = false;
+      goalvisited[i][j] = false;
+    }
+  }
+
+
+
+  int startX = 0, startY = 0;
+  Stack path;
+
+  visited[startY][startX] = true;
+  path.push(startX, startY, 0);
+  
+  int foundGoals = 0; // 已找到目標數量
+  bool success = false; // 是否找到足夠目標
+  while(!path.isEmpty()) {
+    StackNode* topNode = path.getTop();
+    int x = topNode->x;
+    int y = topNode->y;
+    bool moved = false;
+    
+    if (foundGoals >= n) {
+      success = true;
+      break;
+    }
+
+    for (int i = 0; i < 4; i++) {
+      int nx = x + dx[i];
+      int ny = y + dy[i];
+      if (nx < 0 || nx >= width || ny < 0 || ny >= height) {
+        continue;
+      }
+
+      char cell = maze.getbox(nx, ny);
+      if ((cell == 'E' || cell == 'G') && !visited[ny][nx]) {
+        visited[ny][nx] = true;
+        path.getTop()->preferredDir = i + 1;
+        path.push(nx, ny, 0);
+        moved = true;
+        if (cell == 'G' && !goalvisited[ny][nx]) {
+          goalvisited[ny][nx] = true;
+          foundGoals++;
+          break;
+        }
+        break;
+      }
+    }
+  }
 }
 
 void countAllGoals() { // task 3
